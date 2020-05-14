197fd0481291f40ab1bc6eed87446afbe608d7b8
Atanas Angelov <hi@atanas.dev>
2020-05-14 18:56:52 +0300

    Add loaded WP Emerge version checking

--- wpemerge.php
+++ wpemerge.php
@@ -13,11 +13,27 @@
  * Text Domain: myapp
  * Domain Path: /languages
  *
+ * YOU SHOULD NORMALLY NOT NEED TO ADD ANYTHING HERE - any custom functionality unrelated
+ * to bootstrapping the theme should go into a service provider or a separate helper file
+ * (refer to the directory structure in README.md).
+ *
  * @package MyApp
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
+}
+
+// Make sure we can load a compatible version of WP Emerge.
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'version.php';
+
+$name = trim( get_file_data( __FILE__, [ 'Plugin Name' ] )[0] );
+$load = myapp_should_load_wpemerge( $name, '0.15.0', '2.0.0' );
+
+if ( ! $load ) {
+	// An incompatible WP Emerge version is already loaded - stop further execution.
+	// myapp_should_load_wpemerge() will automatically add an admin notice.
+	return;
 }
 
 define( 'MYAPP_PLUGIN_FILE', __FILE__ );
@@ -26,6 +42,8 @@ define( 'MYAPP_PLUGIN_FILE', __FILE__ );
 if ( file_exists( __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php' ) ) {
 	require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 }
+
+myapp_declare_loaded_wpemerge( $name, 'theme', __FILE__ );
 
 // Load helpers.
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'MyApp.php';
