05c9635530aa9102187a7f68b54f00a14615d8a4
Atanas Angelov <hi@atanas.dev>
2020-05-23 15:25:06 +0300

    Bump version requirements to 0.16.0

--- wpemerge.php
+++ wpemerge.php
@@ -28,7 +28,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'version.php';
 
 $name = trim( get_file_data( __FILE__, [ 'Plugin Name' ] )[0] );
-$load = my_app_should_load_wpemerge( $name, '0.15.0', '2.0.0' );
+$load = my_app_should_load_wpemerge( $name, '0.16.0', '2.0.0' );
 
 if ( ! $load ) {
 	// An incompatible WP Emerge version is already loaded - stop further execution.
